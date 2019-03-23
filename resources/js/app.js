e13624cda1e24ed73d956e5731ee125cb5d7dc97
Alex P <alex.pantec@gmail.com>
2019-03-23 13:39:11 +0200

    ui

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 require('./bootstrap');
+
+
+$('.todo-checkbox').click(function () {
+    const chk = $(this).find('input[type=checkbox]');
+    chk.prop('checked', !chk.prop('checked'));
+});
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -5,51 +5,65 @@
         <div class="row h-100">
             <aside class="col-md-3 p-0">
                 <nav class="navbar navbar-expand-md navbar-light">
-                    <button class="navbar-toggler categories-list-toggler" type="button" data-toggle="collapse" data-target="#collapseExample" aria-controls="collapseExample" aria-expanded="false" aria-label="Toggle navigation">
+                    <button class="navbar-toggler categories-list-toggler" type="button" data-toggle="collapse"
+                            data-target="#categoriesList" aria-controls="categoriesList" aria-expanded="false"
+                            aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span> <span>Main</span>
                     </button>
-                        <div class="categories-list list-group collapse navbar-collapse" id="collapseExample">
-                            <a href="#" class="list-group-item list-group-item-action">
-                                Link
-                            </a>
-                            <a href="#" class="list-group-item list-group-item-action">
-                                Link 1
-                            </a>
-                            <a href="#" class="list-group-item list-group-item-action active">
-                                Link
-                            </a>
-                            <a href="#" class="list-group-item list-group-item-action">
-                                Link
-                            </a>
-                            <a href="#" class="list-group-item list-group-item-action">
-                                Link
-                            </a>
-                            <a href="#" class="list-group-item list-group-item-action">
-                                Link
-                            </a>
-                        </div>
+                    <div class="categories-list list-group collapse navbar-collapse" id="categoriesList">
+                        <a href="#" class="list-group-item list-group-item-action">
+                            Link
+                        </a>
+                        <a href="#" class="list-group-item list-group-item-action">
+                            Link 1
+                        </a>
+                        <a href="#" class="list-group-item list-group-item-action active">
+                            Link
+                        </a>
+                        <a href="#" class="list-group-item list-group-item-action">
+                            Link
+                        </a>
+                        <a href="#" class="list-group-item list-group-item-action">
+                            Link
+                        </a>
+                        <a href="#" class="list-group-item list-group-item-action">
+                            Link
+                        </a>
+                    </div>
                 </nav>
             </aside>
             <main class="col-md-9 py-3">
                 <h2 class="d-none d-md-block">Main</h2>
-                <p>Sriracha biodiesel taxidermy organic post-ironic, Intelligentsia salvia mustache 90's code editing
-                    brunch. Butcher polaroid VHS art party, hashtag Brooklyn deep v PBR narwhal sustainable mixtape
-                    swag wolf squid tote bag. Tote bag cronut semiotics, raw denim deep v taxidermy messenger bag. Tofu
-                    YOLO Etsy, direct trade
-                    ethical Odd Future jean shorts paleo. Forage Shoreditch tousled aesthetic irony, street art organic
-                    Bushwick artisan cliche semiotics ugh
-                    synth chillwave meditation. Shabby chic lomo plaid vinyl chambray Vice. Vice sustainable cardigan,
-                    Williamsburg master cleanse hella DIY 90's blog.</p>
+                <div class="todo-list list-group">
+                    <a href="#" class="list-group-item list-group-item-action">
+                        <div class="d-flex align-items-center">
+                            <div class="todo-checkbox align-self-stretch d-flex align-items-center">
+                                <div class="custom-control-lg custom-control custom-checkbox custom-control-inline">
+                                    <input type="checkbox" class="custom-control-input" id="customCheck1">
+                                    <label class="custom-control-label" for="customCheck1"></label>
+                                </div>
+                            </div>
+                            <span>Link</span>
 
-                <p>Ethical Kickstarter PBR
-                    asymmetrical lo-fi. Dreamcatcher street art Carles, stumptown gluten-free Kickstarter artisan
-                    Wes Anderson wolf pug. Godard sustainable you probably haven't heard of them, vegan farm-to-table
-                    Williamsburg slow-carb
-                    readymade disrupt deep v. Meggings seitan Wes Anderson semiotics, cliche American Apparel whatever.
-                    Helvetica cray plaid, vegan brunch Banksy
-                    leggings +1 direct trade. Wayfarers codeply PBR selfies. Banh mi McSweeney's Shoreditch selfies,
-                    forage fingerstache food truck occupy YOLO Pitchfork fixie iPhone fanny pack art party Portland.</p>
+                        </div>
+                    </a>
+                    <a href="#" class="list-group-item list-group-item-action">
+                        <div class="d-flex align-items-center">
+                            <div class="todo-checkbox align-self-stretch d-flex align-items-center">
+                                <div class="custom-control-lg custom-control custom-checkbox custom-control-inline">
+                                    <input type="checkbox" class="custom-control-input" id="customCheck2">
+                                    <label class="custom-control-label" for="customCheck2"></label>
+                                </div>
+                            </div>
+                            <span>Link ssssssssssss      xxxxxxxxxxxxs    fc           s    89c9c      ssssssssss sssssssssssssssssssss s              rrrrrrrrr      sssss sssssssssss fsssssssss</span>
+
+                        </div>
+                    </a>
+                </div>
             </main>
         </div>
     </div>
+
+    <script>
+    </script>
 @endsection
